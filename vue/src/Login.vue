bccc4fde4aae18583c3cbf16127d1127cd20f8c3
cuiym <cym2018.xyz@qq.com>
2020-02-06 22:28:17 +0800

    添加vue.js cli(?)

---
+++ vue/src/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="欢迎"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    components: {
+    }
+  }
+</script>
+
+<style>
+
+</style>
